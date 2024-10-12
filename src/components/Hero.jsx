58e191e3b696af34baf44e4b573b5ff625a5b2a0
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-10-12 08:59:03 +0300

    fix some bugs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -41,7 +41,8 @@ const Hero = () => {
         </div>
       </div>
 
-      {!isMobile && <ComputersCanvas />}
+      {/* {!isMobile && <ComputersCanvas />} */}
+      <ComputersCanvas />
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
